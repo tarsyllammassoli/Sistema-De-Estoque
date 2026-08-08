12141c362f2bbf8abd0b7b99d1693528865dc420
Társylla <contatotaymassoli@gmail.com>
2026-08-08 15:55:20 -0300

    +Testes

--- SistemaEstoque/database/Testes01.sql
+++ SistemaEstoque/database/Testes01.sql
@@ -2,6 +2,7 @@ select * from Categorias
 select * from Fornecedores
 select * from Produtos
 select * from Usuarios
+select * from Movimentacoes_Estoque
 
 update Produtos
 set status_produto = 'Inativo'
@@ -53,3 +54,120 @@ where status_usuario = 'Inativo'
 select data_movimentacao
 from Movimentacoes_Estoque
 where data_movimentacao > '2026-07-15'
+
+--------------------------------------
+
+select top 5 id_produto, nome as 'Produto'
+from Produtos
+order by id_produto asc
+
+select top 10 nome as 'Produtos', preco_venda as 'Preço de Venda'
+from Produtos
+order by preco_venda desc
+
+select top 3 nome as 'Produtos', quantidade
+from Produtos
+order by quantidade asc
+
+select top 5 nome as 'Produtos', preco_venda as 'Preço de Venda'
+from Produtos
+order by preco_venda desc
+
+select top 5 nome as 'Produtos', codigo, preco_venda as 'Preço de Venda'
+from Produtos
+order by preco_venda asc
+
+select distinct status_produto
+from produtos
+
+select distinct tipo_movimentacao
+from Movimentacoes_Estoque
+
+select distinct id_fornecedor, id_categoria
+from produtos
+
+select distinct status_produto
+from produtos
+order by status_produto DESC
+
+-----------------------------
+select * from Movimentacoes_Estoque
+select * from Usuarios
+-----------------------------
+
+select distinct u.id_usuario, m.id_movimentacoes
+from Movimentacoes_Estoque m
+join Usuarios u
+on m.id_usuario = u.id_usuario
+order by id_usuario asc
+
+----------------------------
+select * from Produtos
+select * from Categorias
+----------------------------
+
+select TOP 3 p.nome, c.nome, p.preco_venda
+from produtos p
+join categorias c
+on p.id_categoria = c.id_categoria
+where c.id_categoria = 5
+order by p.preco_venda asc
+
+select TOP 5 p.nome as 'Produto', p.preco_venda, f.nome as 'Fornecedor'
+from produtos p
+join Fornecedores f
+on p.id_fornecedor = f.id_fornecedor
+where f.id_fornecedor = 2
+order by p.preco_venda desc
+
+select TOP 5 p.nome as 'Produto', p.quantidade, p.status_produto
+from Produtos p
+order by quantidade desc
+
+select TOP 1 nome, preco_venda
+from Produtos
+order by preco_venda desc
+
+select TOP 1 nome, preco_venda
+from Produtos
+order by preco_venda asc
+
+select top 5 nome, quantidade
+from Produtos
+order by quantidade asc
+
+select distinct tipo_movimentacao
+from Movimentacoes_Estoque
+
+select distinct p.nome, id_fornecedor
+from produtos p
+order by id_fornecedor desc
+
+select nome as 'Produto', preco_venda
+from Produtos
+where preco_venda > 1000.00
+
+select top 10 nome, preco_venda
+from Produtos
+order by preco_venda desc
+
+select top 1 nome, quantidade
+from Produtos
+order by quantidade desc
+
+select top 1 quantidade
+from Produtos
+order by quantidade asc
+
+select c.nome as 'Categoria', p.nome as 'Produto'
+from Produtos p
+join Categorias c
+on p.id_categoria = c.id_categoria
+
+select top 5 nome as 'Produto',
+	estoque_minimo,
+	codigo,
+	preco_compra,
+	preco_venda
+from Produtos
+order by preco_venda desc
