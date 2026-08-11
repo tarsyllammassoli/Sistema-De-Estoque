51c58fee6e0ccf74b0a9b41991c46461fb148caa
Társylla <contatotaymassoli@gmail.com>
2026-08-11 18:25:31 -0300

    Subqueries correlacionadas

--- SistemaEstoque/database/subqueries02.sql
+++ SistemaEstoque/database/subqueries02.sql
@@ -62,3 +62,55 @@ where p.id_categoria in (
 	where preco_venda > 1000
 )
 
+select top 5 nome, preco_venda
+from produtos
+where preco_venda > (
+	select avg(preco_venda)
+	from produtos
+)
+order by preco_venda desc
+
+select nome, quantidade
+from produtos
+where quantidade < (
+	select avg(quantidade)
+	from Produtos
+)
+order by quantidade asc
+
+-- subquery correlacionada
+
+select p.nome, p.preco_venda, p.id_categoria
+from produtos p
+join Categorias c
+on p.id_categoria = c.id_categoria
+where preco_venda > (
+	select avg(preco_venda)
+	from produtos
+	where p.id_categoria = id_categoria -- pegando a media de preços de cada categoria
+)
+
+select * from Fornecedores
+select * from Produtos
+
+select p.nome, p.id_produto, p.id_fornecedor, f.nome
+from produtos p
+join fornecedores f
+on p.id_fornecedor = f.id_fornecedor
+where p.id_fornecedor > (
+	select id_fornecedor, count(*) as quantidade
+	from produtos
+)
+
+----------------------------------------------
+select id_fornecedor, count(*) as quantidade
+from produtos
+group by id_fornecedor
+----------------------------------------------
+
+select avg(quantidade)
+from (
+	select id_fornecedor, count(*) as quantidade
+	from produtos
+	group by id_fornecedor
+) as Contagem
